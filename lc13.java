43bc20e48524bb5a7a901069d5467594fda7152e
arif9353 <ma782318@gmail.com>
2024-01-11 22:38:40 +0530

    Leetcode Day6/100

---
+++ lc13.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution13 {
+    public int romanToInt(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+        int ans = 0;       
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ans -= m.get(s.charAt(i));
+            } else {
+                ans += m.get(s.charAt(i));
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution13 ob = new Solution13();
+        String s = "LVIII";
+        int ans = ob.romanToInt(s);
+        System.out.println(ans);
+    }
+}
