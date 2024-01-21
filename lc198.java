a24defa8f370a3af61afde8182753e0072ed6682
arif9353 <ma782318@gmail.com>
2024-01-21 22:12:46 +0530

    Leetcode Day16/100

---
+++ lc198.java
@@ -0,0 +1,26 @@
+class Solution198 {
+    public static int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return nums[0];
+        } else if (n == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; ++i) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+
+        return dp[n-1];
+    }
+    public static void main(String args[]){
+        int[] nums = {2,7,9,3,1};
+        System.out.println(rob(nums));
+    }
+}
